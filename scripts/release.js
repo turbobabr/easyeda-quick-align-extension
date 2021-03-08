7fe1e57223bceac0aa8714b1e2b8ec85d5596969
Andrey Shakhmin <andrey.shakhmin@gmail.com>
2021-03-09 05:25:39 +0800

    release scripts

--- scripts/release.js
+++ scripts/release.js
@@ -1,15 +1,17 @@
 #! /usr/bin/env node
+const zipper = require("zip-local");
 const path = require('path');
 const del = require('del');
 const Utils = require('./utils');
 
-const buildDownloadFileName = (version) => {
 
+const buildDownloadFileName = (version) => {
+  return `easyeda-quick-align-${version}.zip`
 };
 
 const buildDownloadURL = (version) => {
-  
-}
+  return `https://github.com/turbobabr/easyeda-quick-align-extension/releases/download/${version}/${buildDownloadFileName(version)}`;
+};
 
 
 (() => {
@@ -20,7 +22,11 @@ const buildDownloadURL = (version) => {
   Utils.createDir(outputDir);
 
   const package = Utils.readJSON(packageFilePath);
+  const version = package.version;
   console.log(package.version);
-  
+
+  const outputFileName = buildDownloadFileName(version);
+  zipper.sync.zip(path.resolve(__dirname,'../build/')).compress().save(path.join(outputDir,outputFileName));
+  console.log('done!');  
 })();
 
